48dd8307250e29cc0cc47909b91d74ea8ab2fb59
Brian <bditmeijer@roc-dev.com>
2016-09-09 15:20:43 +0200

    Eerste opdracht.

---
+++ src/h02/Klasse2.java
@@ -0,0 +1,18 @@
+package h02;
+
+import java.applet.Applet;
+import java.awt.*;
+
+/**
+ * Created by Brian on 8-9-2016.
+ */
+public class Klasse2 extends Applet {
+
+    public void init() {
+
+    }
+
+    public void paint(Graphics g) {
+        g.drawString("Welcome to Java!!", 50, 60 );
+    }
+}
